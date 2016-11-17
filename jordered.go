857e61e9e78f0a8299a649809847130a01a5d674
Jono MacDougall <jonomacd@gmail.com>
2016-11-17 14:08:04 +0000

    first commit

---
+++ jordered.go
@@ -0,0 +1,123 @@
+package jordered
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type OrderedMap struct {
+	ordered []element
+	iter    int
+}
+
+type element struct {
+	key   string
+	value interface{}
+}
+
+// UnmarshalsJSON unmarshals json data and maintains the map order
+func (m *OrderedMap) UnmarshalJSON(data []byte) error {
+	dec := json.NewDecoder(bytes.NewReader(data))
+	m.ordered = []element{}
+
+	// We are only marshalling the first level. Anything deeper we just throw in an interface
+	depth := 0
+
+	for {
+		t, err := dec.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if depth == 0 {
+			switch val := t.(type) {
+			case json.Delim:
+				if strings.ContainsAny(val.String(), "[]") {
+					// This is an array not a map.
+					return fmt.Errorf("Unable to unmarshall array into map")
+				}
+				depth++
+				continue
+			case string, float64, bool, nil, json.Number:
+				return fmt.Errorf("Unable to unmarshall value into map")
+			}
+		}
+
+		if depth == 1 {
+			key, ok := t.(string)
+			if !ok {
+				end, ok := t.(json.Delim)
+				if !ok {
+					return fmt.Errorf("Object Key must be a string %T, %v ", t, t)
+				}
+				if end.String() == "}" {
+					// We are done
+					break
+				}
+			}
+			var value interface{}
+			err := dec.Decode(&value)
+			if err != nil {
+				return err
+			}
+			m.ordered = append(m.ordered, element{
+				key:   key,
+				value: value,
+			})
+		}
+	}
+
+	return nil
+}
+
+// Next returns true if there is a next item
+func (m *OrderedMap) Next() bool {
+	return m.iter < len(m.ordered)
+}
+
+// Item returns the next item in the map, It returns nil if you have reached the end
+func (m *OrderedMap) Item() (string, interface{}) {
+	if m.iter >= len(m.ordered) {
+		return "", nil
+	}
+	item := m.ordered[m.iter]
+	m.iter++
+
+	return item.key, item.value
+}
+
+// Reset restarts the iterator on the map back to the start
+func (m *OrderedMap) Reset() {
+	m.iter = 0
+}
+
+func (m *OrderedMap) Len() int {
+	return len(m.ordered)
+}
+
+func (m *OrderedMap) Get(key string) (interface{}, bool) {
+	for _, item := range m.ordered {
+		if item.key == key {
+			return item.value, true
+		}
+	}
+
+	return nil, false
+}
+
+func (m *OrderedMap) Append(key string, value interface{}) {
+	if m.ordered == nil {
+		m.ordered = []element{}
+	}
+
+	m.ordered = append(m.ordered, element{
+		key:   key,
+		value: value,
+	})
+}
---
+++ jordered_test.go
@@ -0,0 +1,144 @@
+package jordered
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+const (
+	mapJson = `
+{
+    "one": {
+        "hendrik": "sedin" 
+    },
+    "two": {
+        "daniel": "sedin"
+    },
+    "three": [
+        "vancouver",
+        "canucks"
+    ],
+    "four": "hockey"
+}`
+	arrJson = `
+[
+    {
+        "hendrik": "sedin" 
+    },
+    {
+        "daniel": "sedin"
+    },
+    [
+        "vancouver",
+        "canucks"
+    ],
+    "hockey"
+]
+`
+)
+
+func TestOrderedMap(t *testing.T) {
+	om := &OrderedMap{}
+
+	err := json.Unmarshal([]byte(mapJson), om)
+	if err != nil {
+		t.Error(err)
+	}
+
+	keys := []string{"one", "two", "three", "four"}
+	values := [][]byte{
+		[]byte(`{"hendrik":"sedin"}`),
+		[]byte(`{"daniel":"sedin"}`),
+		[]byte(`["vancouver","canucks"]`),
+		[]byte(`"hockey"`),
+	}
+
+	// Test iterator and order
+	ii := 0
+	for om.Next() {
+		key, value := om.Item()
+		if keys[ii] != key {
+			t.Errorf("Item does not have the correct key expected: %s got: %s", keys[ii], key)
+		}
+
+		valB, err := json.Marshal(value)
+		if err != nil {
+			t.Error(err)
+		}
+		if string(values[ii]) != string(valB) {
+			t.Errorf("Item does not have the correct values expected: %s got: %s", string(values[ii]), string(valB))
+		}
+
+		ii++
+	}
+	if ii != 4 {
+		t.Errorf("Did not find all the values (or two many). Expected %v, Got %v", 4, ii)
+	}
+
+	// Test Reset
+	om.Reset()
+	ii = 0
+	for om.Next() {
+		key, value := om.Item()
+		if keys[ii] != key {
+			t.Errorf("Item does not have the correct key expected: %s got: %s", keys[ii], key)
+		}
+
+		valB, err := json.Marshal(value)
+		if err != nil {
+			t.Error(err)
+		}
+		if string(values[ii]) != string(valB) {
+			t.Errorf("Item does not have the correct values expected: %s got: %s", string(values[ii]), string(valB))
+		}
+
+		ii++
+	}
+	if ii != 4 {
+		t.Errorf("Did not find all the values (or two many). Expected %v, Got %v", 4, ii)
+	}
+	om.Reset()
+	// Test append
+	om.Append("five", 127)
+	keys = append(keys, "five")
+	values = append(values, []byte(`127`))
+	ii = 0
+	for om.Next() {
+		key, value := om.Item()
+		if keys[ii] != key {
+			t.Errorf("Item does not have the correct key expected: %s got: %s", keys[ii], key)
+		}
+
+		valB, err := json.Marshal(value)
+		if err != nil {
+			t.Error(err)
+		}
+		if string(values[ii]) != string(valB) {
+			t.Errorf("Item does not have the correct values expected: %s got: %s", string(values[ii]), string(valB))
+		}
+
+		ii++
+	}
+	if ii != 5 {
+		t.Errorf("Did not find all the values (or two many). Expected %v, Got %v", 5, ii)
+	}
+
+	value, ok := om.Get("two")
+	if !ok {
+		t.Errorf("Key two does not exist, it should")
+	}
+	valB, err := json.Marshal(value)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(values[1]) != string(valB) {
+		t.Errorf("Item does not have the correct values expected: %s got: %s", string(values[1]), string(valB))
+	}
+
+	om = &OrderedMap{}
+
+	err = json.Unmarshal([]byte(arrJson), om)
+	if err == nil {
+		t.Error("Marshalling should have failed. It succeeded")
+	}
+}
